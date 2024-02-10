9ed50f237a052dacbbcd3993c2339c4291772c33
Jitu7 <Jyotirmaya12@gmail.com>
2024-02-10 19:31:56 +0530

    added dto mapper for filtration purpose for client...but still loads all the fields of entity

--- bookmarker-api/src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
+++ bookmarker-api/src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.example.bookmarkerapi.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -13,11 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookmarkService {
 
     private final BookmarkRepository repository;
+    private final BookmarkMapper mapper;
 
     @Transactional(readOnly = true)
     public BookmarksDTO getBookmarks(Integer page) {
         int pageNo = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
-        return new BookmarksDTO(repository.findAll(pageable));
+        Page<BookmarkDTO> bookmarkDTOPage = repository.findAll(pageable).map(mapper::toDTO);
+        return new BookmarksDTO(bookmarkDTOPage);
     }
 }
