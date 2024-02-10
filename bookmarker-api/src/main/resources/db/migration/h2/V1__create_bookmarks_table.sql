89ba7c17d8eac0a4b19d22443c4f4718d163c8d9
Jitu7 <Jyotirmaya12@gmail.com>
2024-02-10 19:07:31 +0530

    initial data set up, added pagination, added flyway migration

---
+++ bookmarker-api/src/main/resources/db/migration/h2/V1__create_bookmarks_table.sql
@@ -0,0 +1,10 @@
+create sequence if not exists bm_id_seq start with 1 increment by 50;
+
+create table if not exists bookmarks
+(
+    id         bigint default nextval('bm_id_seq') not null,
+    title      varchar(255)                        not null,
+    url        varchar(255)                        not null,
+    created_at timestamp,
+    primary key (id)
+);
