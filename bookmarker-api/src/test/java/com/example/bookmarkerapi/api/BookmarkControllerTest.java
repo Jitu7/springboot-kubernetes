b4af8b5f1d424861df15149b5d89fe7f7b16f1a1
Jitu7 <Jyotirmaya12@gmail.com>
2024-02-10 20:43:03 +0530

    added testcontainers support for postgres and added parameterized test

--- bookmarker-api/src/test/java/com/example/bookmarkerapi/api/BookmarkControllerTest.java
+++ bookmarker-api/src/test/java/com/example/bookmarkerapi/api/BookmarkControllerTest.java
@@ -2,12 +2,15 @@ package com.example.bookmarkerapi.api;
 
 import com.example.bookmarkerapi.domain.Bookmark;
 import com.example.bookmarkerapi.domain.BookmarkRepository;
+import lombok.SneakyThrows;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
@@ -20,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:latest:///demo"
+})
 class BookmarkControllerTest {
 
     @Autowired
@@ -53,17 +59,24 @@ class BookmarkControllerTest {
         repository.saveAll(bookmarks);
     }
 
-    @Test
-    void shouldGetBookMarks() throws Exception {
-        mockMvc.perform(get("/api/bookmarks"))
+    @ParameterizedTest
+    @CsvSource({
+            "1,14,2,1,true,false,true,false",
+            "2,14,2,2,false,true,false,true",
+
+    })
+    @SneakyThrows
+    void shouldGetBookMarks(int pageNo, int totalElements, int totalPages, int currentPage, boolean isFirst,
+                            boolean isLast, boolean hasNext, boolean hasPrevious) {
+        mockMvc.perform(get("/api/bookmarks?page=" + pageNo))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.totalElements", CoreMatchers.equalTo(14)))
-                .andExpect(jsonPath("$.totalPages", CoreMatchers.equalTo(2)))
-                .andExpect(jsonPath("$.currentPage", CoreMatchers.equalTo(1)))
-                .andExpect(jsonPath("$.isFirst", CoreMatchers.equalTo(true)))
-                .andExpect(jsonPath("$.isLast", CoreMatchers.equalTo(false)))
-                .andExpect(jsonPath("$.hasNext", CoreMatchers.equalTo(true)))
-                .andExpect(jsonPath("$.hasPrevious", CoreMatchers.equalTo(false)))
+                .andExpect(jsonPath("$.totalElements", CoreMatchers.equalTo(totalElements)))
+                .andExpect(jsonPath("$.totalPages", CoreMatchers.equalTo(totalPages)))
+                .andExpect(jsonPath("$.currentPage", CoreMatchers.equalTo(currentPage)))
+                .andExpect(jsonPath("$.isFirst", CoreMatchers.equalTo(isFirst)))
+                .andExpect(jsonPath("$.isLast", CoreMatchers.equalTo(isLast)))
+                .andExpect(jsonPath("$.hasNext", CoreMatchers.equalTo(hasNext)))
+                .andExpect(jsonPath("$.hasPrevious", CoreMatchers.equalTo(hasPrevious)))
         ;
     }
 
